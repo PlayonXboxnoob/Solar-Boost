365fc1cd8c05e45dd7302bab316c22a80410de8f
Aditya Naravane <adityanaravane2008@gmail.com>
2022-10-29 19:00:57 -0400

    Initial Commit

---
+++ Solar Boost/Solar_BoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_BoostApp.swift
+//  Solar Boost
+//
+//  Created by Pallavi Naravane on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Solar_BoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
