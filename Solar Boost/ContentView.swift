f339fffb6df5ad23aef17d27fbbd2808d438c90a
Aditya Naravane <adityanaravane2008@gmail.com>
2022-10-30 07:50:26 -0400

    Finally some success!

--- Solar Boost/ContentView.swift
+++ Solar Boost/ContentView.swift
@@ -7,20 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ContentView: View{
+    
+    var body: some View{
+        MainView()
     }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{
         ContentView()
     }
 }
